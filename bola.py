3ef62d8a59f535f4df1c16832c81cb70abfe22d3
rogerio-pio <rogeriomachadopio@gmail.com>
2025-05-19 00:23:37 -0300

    add bola e integração campo

--- bola.py
+++ bola.py
@@ -1,19 +1,19 @@
 import numpy as np
 from OpenGL.GL import *
 
-def _draw_sphere(radius, slices, stacks):
-    for i in range(stacks):
-        lat0 = np.pi * (-0.5 + i / stacks)
-        lat1 = np.pi * (-0.5 + (i + 1) / stacks)
-        z0, zr0 = radius * np.sin(lat0), radius * np.cos(lat0)
-        z1, zr1 = radius * np.sin(lat1), radius * np.cos(lat1)
+def bola(raio, lados, pilhas):
+    for i in range(pilhas):
+        lat0 = np.pi * (-0.5 + i / pilhas)
+        lat1 = np.pi * (-0.5 + (i + 1) / pilhas)
+        z0, zr0 = raio * np.sin(lat0), raio * np.cos(lat0)
+        z1, zr1 = raio * np.sin(lat1), raio * np.cos(lat1)
 
         glBegin(GL_QUAD_STRIP)
-        for j in range(slices + 1):
+        for j in range(lados + 1):
             v = ((i // 5) + (j // 5)) % 2
             glColor3f(v, v, v)
 
-            lng = 2 * np.pi * j / slices
+            lng = 2 * np.pi * j / lados
             x, y = np.cos(lng), np.sin(lng)
 
             glNormal3f(x * zr0, y * zr0, z0)
@@ -23,8 +23,8 @@ def _draw_sphere(radius, slices, stacks):
             glVertex3f(x * zr1, y * zr1, z1)
         glEnd()
 
-def draw_ball(pos=(0.0, 0.3, 0.0), radius=0.3):
+def draw_ball(pos=(0.0, 0.3, 0.0), raio=0.3):
     glPushMatrix()
     glTranslatef(*pos)
-    _draw_sphere(radius, 50, 50)
+    bola(raio, 50, 50)
     glPopMatrix()
